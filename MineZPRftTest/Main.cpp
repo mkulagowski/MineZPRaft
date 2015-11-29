3f195f45606005d1a683e9e823ebef7bfb1b69e2
Lukasz Kostyra <costyrra.xl@gmail.com>
2015-11-29 10:52:21 +0100

    Add Google Test to project
    Following is done in this commit:
    * Gtest project is added
    * MineZPRftTest project is created
    * Minor changes are done to exclude OpenGL from MineZPRft/Common
    
    Since MineZPRft is buildable in executable form, testing its units
    is slightly different. We don't have our units in library form, so
    we must recompile them as a part of MineZPRftTest.
    
    Visual Studio .vcxproj assumes following structure to avoid file
    duplication:
    * Tests are to be created in root project filter
    * Filter "Requirements" is for all needed modules from MineZPRft
      (now it is Common/FileSystem and Common/Common)
    * Filter "Units" is a place to add .cpp files from MineZPRft project
      which are to be tested.
    
    Similar maneuvers are done in CMakeLists.txt.
    
    Verification:
        Build all.
    
    Change-Id: Ic6fc3bd8e40a0bfb24a079c686da150ae25b355d

---
+++ MineZPRftTest/Main.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @author LKostyra (costyrra.xl@gmail.com)
+ * @brief  Main file for MineZPRft Tests
+ */
+
+#include <gtest/gtest.h>
+#include <Common/FileSystem.hpp>
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    FS::ChangeDirectory(FS::GetExecutableDir() + "/../../..");
+    int result = RUN_ALL_TESTS();
+
+#if defined(WIN32) && defined(_CRTDBG_MAP_ALLOC)
+    _CrtDumpMemoryLeaks();
+#endif // defined(WIN32) && defined(_CRTDBG_MAP_ALLOC)
+
+    return result;
+}

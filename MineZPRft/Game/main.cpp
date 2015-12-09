152be9d3c7f11489948214c917d59ce108dcde53
mkkulagowski <mk.kulagowski@gmail.com>
2015-12-09 15:06:42 +0100

    Logger class implementation
    Implements Logger class for Windows & Linux.
    Updates .gitignore file to ignore produced log files.
    Uses Logger in FileSystem.cpp file, to show proper usage.
    Adds GetCWD function to FileSystem and uses it in main,
    to make filename in logs shorter.
    
    Verification: Build and run MineZPRft. First line should be LOG_INFO from
    FileSystem class. Every log after this one should have shorter filename.
    
    Change-Id: Iad1fd63cfd9ef688c323b1aff269dd9b941dd2c0

--- MineZPRft/Game/main.cpp
+++ MineZPRft/Game/main.cpp
@@ -7,12 +7,21 @@
 #include "GameManager.hpp"
 
 #include "Common/FileSystem.hpp"
+#include "Common/Logger.hpp"
 
 int main()
 {
+    // Note: logs from here on will have their filename unstripped
+
     // Navigate to project root directory
     FS::ChangeDirectory(FS::GetExecutableDir() + "/../../..");
 
+    // Give logger CWD, so the logs from here on have their filename stripped
+    Logger::GetInstance().SetCurrentWorkingDir(FS::GetCurrentWorkingDir());
+
+    // Note: logs from here on will have their filename stripped
+
+    // Start the main game loop
     GameManager::GetInstance().GameLoop();
 
     return 0;

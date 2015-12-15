2a5ca1fc4859a69ec2a15f654bd991f74f5a5510
Lukasz Kostyra <costyrra.xl@gmail.com>
2015-12-15 12:21:16 +0100

    Terrain: Implement Voxel Database
    Introduced is the Voxel Database, the one place to keep all Voxel
    data and configurations for convenience.
    
    For now, the only Voxel attribute kept there is Voxel color in RGB
    form. Terrain Generator gets the data from Voxel Database and applies
    it during VBO creation.
    
    Verification:
        Build, run. Check if chunk color can be changed - the only
        modification to do should be done inside the Voxel DB map.
    
    Change-Id: Iff46a75fb89d429c100a9c2b783d388fad69062d

---
+++ MineZPRft/Terrain/VoxelDatabase.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file
+ * @author LKostyra (costyrra.xl@gmail.com)
+ * @brief  Voxel Database declarations.
+ */
+
+#ifndef __TERRAIN_VOXELDATABASE_HPP__
+#define __TERRAIN_VOXELDATABASE_HPP__
+
+#include <map>
+
+/**
+ * Enumerates voxels available to set inside a Chunk.
+ *
+ * Refer to VoxelDatabase.hpp for more detailed descriptions about each voxel.
+ */
+enum class VoxelType : unsigned char
+{
+    Air = 0,
+    Bedrock,
+    Stone,
+    Unknown
+};
+
+/**
+ * Describes each voxel with a set of attributes specific to it.
+ */
+struct Voxel
+{
+    float colorRed;     ///< Voxel color, Red component
+    float colorGreen;   ///< Voxel color, Green component
+    float colorBlue;    ///< Voxel color, Blue component
+};
+
+
+typedef std::map<VoxelType, Voxel> VoxelDBType;
+
+extern const VoxelDBType VoxelDB;
+
+#endif // __TERRAIN_VOXELDATABASE_HPP__

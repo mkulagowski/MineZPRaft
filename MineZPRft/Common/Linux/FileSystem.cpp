28347e11a15decaaaeb765ed9693b75cc9e585b6
mkkulagowski <mk.kulagowski@gmail.com>
2016-01-17 03:01:47 +0100

    Terrain: Implement Chunk saving
    When chunk is generated, it checks for saved file on the disk.
    If not found, then chunk is generated in a standard way. Otherwise it's
    loaded from the file.
    Chunks are encoded using simple RLE algorithm.
    
    Also added simple addition to FileSystem.
    
    Verification: Build and run.
    
    Change-Id: Ie19985e148893e58ff57fb027664ea13e0d6fc2c

--- MineZPRft/Common/Linux/FileSystem.cpp
+++ MineZPRft/Common/Linux/FileSystem.cpp
@@ -72,4 +72,29 @@ std::string GetCurrentWorkingDir()
     return currPathStr;
 }
 
+bool CreateDir(const std::string& path)
+{
+    if (::mkdir(path.c_str(), 0777) != 0)
+    {
+        LOG_E("Failed to create directory '" << path.c_str() << "' : "
+                  << strerror(errno));
+        return false;
+    }
+
+    LOG_I("Created directory '" << path.c_str() << "'");
+    return true;
+}
+
+bool IsDir(const std::string& path)
+{
+    struct stat stat;
+    if (::stat(path.c_str(), &stat) != 0)
+        return false;
+
+    if (S_ISDIR(stat.st_mode))
+        return true;
+
+    return false;
+}
+
 } // namespace FS
